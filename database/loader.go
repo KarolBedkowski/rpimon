720601a7dd272df7ee6e3f8d8437f37b92046f18
Karol Będkowski <karol.bedkowski@gmail.com>
2013-12-08 21:45:08 +0100

    konfiguracja użytkowników w pliku json

--- database/loader.go
+++ database/loader.go
@@ -1,44 +1,41 @@
 package database
 
 import (
-	"encoding/csv"
-	"io"
+	"encoding/json"
+	"io/ioutil"
 	l "k.prv/rpimon/helpers/logging"
-	"os"
-	"strings"
 )
 
-var users = make(map[string]*User, 5)
+type UsersConfig struct {
+	Users []User
+}
+
+var users UsersConfig
 
 func Init(usersFile string, debug bool) {
 	LoadUsers(usersFile)
 }
 
 func LoadUsers(filename string) error {
-	file, err := os.Open(filename)
+	l.Info("LoadUsers from: %s", filename)
+	file, err := ioutil.ReadFile(filename)
 	if err != nil {
-		l.Error("Error:", err)
+		l.Error("LoadUsers Error:", err)
 		return err
 	}
-	defer file.Close()
-	reader := csv.NewReader(file)
-	reader.Comma = ';'
-	for {
-		record, err := reader.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			l.Error("Error:", err)
-			return err
-		}
-		user := &User{Login: record[0],
-			Password: record[1],
-			Privs:    strings.Split(record[2], " ")}
-		users[user.Login] = user
+	err = json.Unmarshal(file, &users)
+	if err != nil {
+		l.Error("LoadUsers Error:", err)
 	}
-	return nil
+	l.Info("LoadUsers loaded: %d", len(users.Users))
+	return err
 }
 
 func SelectOneUser(login string) *User {
-	return users[login]
+	for _, user := range users.Users {
+		if user.Login == login {
+			return &user
+		}
+	}
+	return nil
 }
