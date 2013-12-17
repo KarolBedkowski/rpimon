afc8aadf54133a20db122644981d034a9d77bfb2
Karol Będkowski <karol.bedkowski@gmail.com>
2013-12-17 18:27:10 +0100

    task do czyszczenia starych sesji

--- server.go
+++ server.go
@@ -8,12 +8,14 @@ import (
 	pmain "k.prv/rpimon/pages/main"
 	pmpd "k.prv/rpimon/pages/mpd"
 	pnet "k.prv/rpimon/pages/net"
+	pproc "k.prv/rpimon/pages/process"
 	pstorage "k.prv/rpimon/pages/storage"
 	pusers "k.prv/rpimon/pages/users"
 	putils "k.prv/rpimon/pages/utils"
 	"log"
 	"net/http"
-	_ "net/http/pprof"
+	//	_ "net/http/pprof"
+	"time"
 )
 
 func main() {
@@ -36,6 +38,23 @@ func main() {
 	pmpd.CreateRoutes(app.Router.PathPrefix("/mpd"))
 	plogs.CreateRoutes(app.Router.PathPrefix("/logs"))
 	pusers.CreateRoutes(app.Router.PathPrefix("/users"))
+	pproc.CreateRoutes(app.Router.PathPrefix("/process"))
+
+	go app.ClearSessionStore()
+	// clear session task
+	ticker := time.NewTicker(time.Hour)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				app.ClearSessionStore()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
 
 	log.Printf("Listen: %s", *httpAddr)
 	if err := http.ListenAndServe(*httpAddr, nil); err != nil {
