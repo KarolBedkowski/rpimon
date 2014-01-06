e6f338cdef7a0b3008ebdcad561a28fc4f268eda
Karol Będkowski <karol.bedkowski@gmail.com>
2014-01-06 11:46:50 +0100

    mpd: usuniecie actions; zmiana nazwy zakładki info->status

--- pages/mpd/mpd.go
+++ pages/mpd/mpd.go
@@ -47,10 +47,6 @@ func CreateRoutes(parentRoute *mux.Route) {
 	subRouter.HandleFunc("/service/info",
 		app.VerifyPermission(infoHandler, "mpd")).Name(
 		"mpd-service-info")
-	// MPD actions
-	subRouter.HandleFunc("/actions",
-		app.VerifyPermission(actionsPageHandler, "mpd")).Name(
-		"mpd-actions")
 	// Library
 	subRouter.HandleFunc("/library",
 		app.VerifyPermission(libraryPageHandler, "mpd")).Name(
@@ -71,12 +67,11 @@ var localMenu []*app.MenuItem
 
 func createLocalMenu() []*app.MenuItem {
 	if localMenu == nil {
-		localMenu = []*app.MenuItem{app.NewMenuItemFromRoute("Info", "mpd-index"),
+		localMenu = []*app.MenuItem{app.NewMenuItemFromRoute("Status", "mpd-index"),
 			app.NewMenuItemFromRoute("Playlist", "mpd-playlist"),
 			app.NewMenuItemFromRoute("Library", "mpd-library"),
 			app.NewMenuItemFromRoute("Playlists", "mpd-playlists"),
 			app.NewMenuItemFromRoute("Log", "mpd-log"),
-			app.NewMenuItemFromRoute("Actions", "mpd-actions"),
 		}
 	}
 	return localMenu
@@ -156,12 +151,6 @@ func infoHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 const fakeResult = `{"Status":{"consume":"0","mixrampdb":"0.000000","mixrampdelay":"nan","nextsong":"1","nextsongid":"1","playlist":"2","playlistlength":"222","random":"0","repeat":"0","single":"0","song":"0","songid":"0","state":"stop","volume":"100","xfade":"0"},"Current":{"Album":"Café Del Mar - Classic I","Artist":"Jules Massenet","Date":"2002","Genre":"Baroque, Modern, Romantic, Classical","Id":"0","Last-Modified":"2013-09-27T06:14:59Z","Pos":"0","Time":"312","Title":"Meditation","Track":"01/12","file":"muzyka/mp3/cafe del mar/compilations/classics/2002, classic/01. jules massenet - meditation.mp3"},"Error":""}`
-
-func actionsPageHandler(w http.ResponseWriter, r *http.Request) {
-	ctx := newPageCtx(w, r)
-	ctx.CurrentLocalMenuPos = "mpd-actions"
-	app.RenderTemplate(w, ctx, "base", "base.tmpl", "mpd/actions.tmpl", "flash.tmpl")
-}
 
 type libraryPageCtx struct {
 	*app.BasePageContext
