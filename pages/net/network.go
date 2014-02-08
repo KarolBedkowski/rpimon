d4d696509c8ade54e5a56bd48b56bf9623cec89f
Karol Będkowski <karol.bedkowski@gmail.com>
2014-02-08 12:00:21 +0100

    net: podgląd konfiguracji z pol ifconfig, ip, route, arp zamiast tylko ip

--- pages/net/network.go
+++ pages/net/network.go
@@ -15,11 +15,12 @@ import (
 func CreateRoutes(parentRoute *mux.Route) {
 	subRouter := parentRoute.Subrouter()
 	subRouter.HandleFunc("/", app.VerifyPermission(mainPageHandler, "admin")).Name("net-index")
+	subRouter.HandleFunc("/conf", app.VerifyPermission(confPageHandler, "admin")).Name("net-conf")
 	subRouter.HandleFunc("/serv/info", app.VerifyPermission(infoHandler, "admin")).Name("net-serv-info")
 	subRouter.HandleFunc("/{page}", app.VerifyPermission(subPageHandler, "admin")).Name("net-page")
 	localMenu = []*app.MenuItem{
 		app.NewMenuItemFromRoute("Status", "net-index").SetID("status"),
-		app.NewMenuItemFromRoute("IFConfig", "net-page", "page", "ifconfig").SetID("ifconfig"),
+		app.NewMenuItemFromRoute("Configuration", "net-conf").SetID("conf"),
 		app.NewMenuItemFromRoute("IPTables", "net-page", "page", "iptables").SetID("iptables"),
 		app.NewMenuItemFromRoute("Netstat", "net-page", "page", "netstat").SetID("netstat"),
 		app.NewMenuItemFromRoute("Conenctions", "net-page", "page", "connenctions").SetID("connenctions")}
@@ -44,13 +45,11 @@ func subPageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	page, ok := vars["page"]
 	if !ok {
-		page = "ifconfig"
+		http.Redirect(w, r, app.GetNamedURL("net-index"), http.StatusFound)
 	}
 	data := app.NewSimpleDataPageCtx(w, r, "Network", "net", page, localMenu)
 	data.CurrentLocalMenuPos = page
 	switch page {
-	case "ifconfig":
-		data.Data = h.ReadFromCommand("ip", "addr")
 	case "iptables":
 		data.Data = h.ReadFromCommand("sudo", "iptables", "-L", "-vn")
 	case "netstat":
@@ -61,6 +60,70 @@ func subPageHandler(w http.ResponseWriter, r *http.Request) {
 		data.TData, _ = netstat("sudo", "netstat", "-pn", "--inet", "--inet6")
 	}
 	app.RenderTemplateStd(w, data, "data.tmpl")
+}
+
+type confPageContext struct {
+	*app.BasePageContext
+	Current  string
+	Data     string
+	Commands *[]string
+}
+
+var confCommands = []string{
+	"ifconfig",
+	"route -n",
+	"arp -n",
+	"ip link",
+	"ip addr",
+	"ip addrlabel",
+	"ip route",
+	"ip rule",
+	"ip neigh",
+	"ip ntable",
+	"ip tunnel",
+	"ip tuntap",
+	"ip maddr",
+	"ip mroute",
+	"ip mrule",
+	"ip monitor",
+	"ip xfrm",
+	"ip netns",
+	"ip l2tp",
+	"ip tcp_metrics",
+	"ip token",
+}
+
+func confPageHandler(w http.ResponseWriter, r *http.Request) {
+	cmd := r.FormValue("cmd")
+	if cmd == "" {
+		cmd = confCommands[0]
+	} else {
+		ok := false
+		for _, dcmd := range confCommands {
+			if cmd == dcmd {
+				ok = true
+				break
+			}
+		}
+		if !ok {
+			http.Error(w, "invalid request", http.StatusBadRequest)
+			return
+		}
+	}
+	cmdfields := strings.Fields(cmd)
+
+	if r.FormValue("data") == "1" {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte(h.ReadFromCommand(cmdfields[0], cmdfields[1:]...)))
+	} else {
+		ctx := &confPageContext{BasePageContext: app.NewBasePageContext("Network", "net", w, r)}
+		ctx.CurrentLocalMenuPos = "conf"
+		ctx.LocalMenu = localMenu
+		ctx.Current = cmd
+		ctx.Commands = &confCommands
+		ctx.Data = h.ReadFromCommand(cmdfields[0], cmdfields[1:]...)
+		app.RenderTemplateStd(w, ctx, "net/conf.tmpl")
+	}
 }
 
 func netstat(command string, args ...string) ([][]string, error) {
