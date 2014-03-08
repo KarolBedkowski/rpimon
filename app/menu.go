636d9dc5569eca1fc960d933fe3a4271d874a576
Karol Będkowski <karol.bedkowski@gmail.com>
2014-03-08 20:33:50 +0100

    przeczyszczenie app/*

--- app/menu.go
+++ app/menu.go
@@ -2,6 +2,7 @@ package app
 
 import "k.prv/rpimon/app/context"
 
+// NewMenuItemFromRoute create new menu item from named route
 func NewMenuItemFromRoute(name string, route string, args ...string) *context.MenuItem {
 	return context.NewMenuItem(name, GetNamedURL(route, args...)).SetID(route)
 }
